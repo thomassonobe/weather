c7485c572d89c721414a7203603ff6a917a473b5
Thomas Chang <thomas.sonobe@gmail.com>
2020-04-27 18:49:57 -0300

    Tracking changes to input

---
+++ components/SearchInput.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native'
+
+export default class SearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChangeText = text => {
+    this.setState({ text });
+  }
+
+  handleSubmitEditing = () => {
+    const { onSubmit } = this.props;
+    const { text } = this.state;
+
+    if (!text) return;
+
+    onSubmit(text);
+    this.setState({ text: '' });
+  };
+
+  render() {
+    const { placeholder } = this.props;
+    const { text } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <TextInput
+          autoCorrect={false}
+          value={text}
+          placeholder={placeholder}
+          placeholderTextColor="#fff"
+          underlineColorAndroid="transparent"
+          style={styles.textInput}
+          clearButtonMode="always"
+          onChangeText={this.handleChangeText}
+          onSubmitEditing={this.handleSubmitEditing}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 40,
+    marginTop: 20,
+    backgroundColor: '#696969',
+    marginHorizontal: 40,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+
+  textInput: {
+    flex: 1,
+    color: '#fff',
+  },
+});
