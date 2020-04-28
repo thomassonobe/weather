ca12afc44aeebdee5d07b9a5ea37318b0ebf1249
Thomas Chang <thomas.sonobe@gmail.com>
2020-04-28 19:10:09 -0300

    Networking

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ import {
   ImageBackground,
   Text,
   KeyboardAvoidingView,
-  Platform
+  Platform,
+  ActivityIndicator,
+  StatusBar,
 } from 'react-native';
 
 
+import { fetchLocationId, fetchWeather } from './utils/api';
 import getImageForWeather from './utils/getImageForWeather';
 import SearchInput from './components/SearchInput';
 
@@ -16,44 +19,85 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: 'San Francisco',
+      loading: false,
+      error: false,
+      location: '',
+      temperature: 0,
+      weather: '',
     };
   }
-  handleUpdateLocation = city => {
-    this.setState({
-      location: city,
-    });
+
+  componentDidMount() {
+    this.handleUpdateLocation('São Paulo');
+  }
+
+  handleUpdateLocation = async city => {
+    if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId,
+        );
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          weather,
+          temperature,
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true,
+        })
+      }
+    })
   };
 
   render() {
-    const { location } = this.state;
+    const { loading, error, location, weather, temperature } = this.state;
     return (
-      <View style={styles.container} behavior="padding" >
+      <View style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
         <ImageBackground
-          source={getImageForWeather('Clear')}
+          source={getImageForWeather(weather)}
           style={styles.imageContainer}
           imageStyle={styles.image}
         >
           <View style={styles.detailsContainer}>
-            <Text style={[styles.largeText, styles.textStyle]}>
-              {location}
-            </Text>
-            <Text style={[styles.smallText, styles.textStyle]}>
-              Céu Aberto
-          </Text>
-            <Text style={[styles.largeText, styles.textStyle]}>
-              28º
-          </Text>
-
-            <SearchInput
-              placeholder="Search any city"
-              onSubmit={this.handleUpdateLocation}
-            />
+            <ActivityIndicator animating={loading} color="white" size="large" />
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different city.
+                  </Text>
+                )}
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {location}
+                    </Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>
+                      {weather}
+                    </Text>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {`${Math.round(temperature)}°`}
+                    </Text>
+                  </View>
+                )}
+                <SearchInput
+                  placeholder="Search any city"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
           </View>
         </ImageBackground>
       </View>
     );
-
   }
 
 };
--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native'
 
+
 export default class SearchInput extends React.Component {
   constructor(props) {
     super(props);
